68118e46ff4fa3ab5fe22e69709d55851f843c38
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-01-11 20:57:08 +0700

    example programs

---
+++ basic/wifi_scan.py
@@ -0,0 +1,8 @@
+import wifi
+networks = []
+for network in wifi.radio.start_scanning_networks():
+    networks.append(network)
+wifi.radio.stop_scanning_networks()
+networks = sorted(networks, key=lambda net: net.rssi, reverse=True)
+for network in networks:
+    print("ssid:",network.ssid, "rssi:",network.rssi)
